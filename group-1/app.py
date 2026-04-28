ddba76ac69c64b64e9afd95ae1bfc08537df1198
Joe Tay <tay.joe@gmail.com>
2026-04-28 10:54:54 +0800

    group-1: upload from portal at 2026-04-28 02:54 UTC

--- group-1/app.py
+++ group-1/app.py
@@ -1,6 +1,4 @@
 import streamlit as st
-
-st.set_page_config(page_title="CTSS Streamlit Project", page_icon="🚀")
-st.title("Welcome, Group!")
-st.write("Your app hasn't been uploaded yet. Head to the upload portal to deploy your code.")
-st.info("Once you upload, this page will be replaced with your Streamlit app within ~60 seconds.")
+st.title("Group 1 - Smoke Test")
+st.write("Deployed via the upload portal!")
+st.balloons()

87af13d3360b9d79346da5716332d272d2e87cb6
Joe Tay <tay.joe@gmail.com>
2026-05-14 12:43:26 +0800

    group-8: upload from portal at 2026-05-14 04:43 UTC

--- group-8/app.py
+++ group-8/app.py
@@ -1,15 +1,19 @@
 import streamlit as st
 
-col1, col2, col3, col4, col5, col6 = st.columns(6)
+col1, col2, col3, col4, col5, col6, col7, col8= st.columns(8)
 with col1:
-    N_1 = st.button("N-1", help = "N-1")
+    N_1 = st.button("_", help = "N-1")
 with col2:
-    N_2 = st.button("N-2", help = "N-2")
+    N_2 = st.button("_", help = "N-2")
 with col3:
-    N_3 = st.button("N-3", help = "N-3")
+    N_3 = st.button("_", help = "N-3")
 with col4:
-    N_1 = st.button("N-4", help = "N-4")
+    N_4 = st.button("_", help = "N-4")
 with col5:
-    N_2 = st.button("N-5", help = "N-5")
+    N_5 = st.button("_", help = "N-5")
 with col6:
-    N_3 = st.button("N-6", help = "N-6")
+    N_6 = st.button("_", help = "N-6")
+with col7:
+    N_7 = st.button("_", help = "N-7")
+with col8:
+    N_8 = st.button("_", help = "N-8")

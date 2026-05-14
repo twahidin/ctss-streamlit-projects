6aa75053e157927ea7f0c39e2ca6fec34aff93f2
Joe Tay <tay.joe@gmail.com>
2026-05-14 10:44:08 +0800

    group-7: upload from portal at 2026-05-14 02:44 UTC

--- group-7/app.py
+++ group-7/app.py
@@ -1,6 +1,35 @@
 import streamlit as st
+status = True
+yes = "work"
+if st.button("plane"):
+    st.write(yes)
+if st.button("no plane"):
+    no = "plane crashe"
+    st.write(no)
+name = st.text_input("Name")
+if name == ' ':
+    st.error("please enter a valid name")
 
-st.set_page_config(page_title="CTSS Streamlit Project", page_icon="🚀")
-st.title("Welcome, Group!")
-st.write("Your app hasn't been uploaded yet. Head to the upload portal to deploy your code.")
-st.info("Once you upload, this page will be replaced with your Streamlit app within ~60 seconds.")
+for loop in range(len(name)):
+    if name[loop].isdigit():
+        status = False
+if status == False:
+    st.warning("why is there number in your name")
+if st.button("balloon"):
+    for loop in range(10):
+      st.balloons()
+
+
+
+import streamlit as st
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("A cat")
+
+with col2:
+    st.header("A dog")
+
+with col3:
+    st.header("An owl")

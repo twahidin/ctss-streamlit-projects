e81139d864b6a3c10828e6a18aa12357348cc9cc
Joe Tay <tay.joe@gmail.com>
2026-05-14 11:16:39 +0800

    group-8: upload from portal at 2026-05-14 03:16 UTC

--- group-8/app.py
+++ group-8/app.py
@@ -1,24 +1,9 @@
-#9/5/26
-#Admin Terminal and authentication for backend
-
 import streamlit as st
 
 col1, col2, col3 = st.columns(3)
+with col1:
+    N_1 = st.button("N-1")
 with col2:
-    st.title(":grey[Silver Kampong Admin Terminal]")
-    st.write("[insert slogan]")
-    container = st.container(border=True)
-    user_name = container.text_input("**Username:**")
-    container.write("")
-    password = container.text_input("**Password:**")
-
+    N_2 = st.button("N-2")
 with col3:
-    login = container.button(":red[**Login**]")
-
-if user_name and password and login:
-    #Connect to backend for real authentication
-    if user_name == "Admin" and password == "1234": #Testing purposes only
-        st.success('Successfully login!', icon="✅")
-    else:
-        st.error(f"Unsuccessful login", icon="🚨")
-        st.error('Your Username or password is incorrect', icon="🚨")
+    N_3 = st.button("N-3")

b493772e5d927468f0999f51c703585d7829e736
Joe Tay <tay.joe@gmail.com>
2026-05-19 16:07:33 +0800

    group-1: upload from portal at 2026-05-19 08:07 UTC

--- group-1/app.py
+++ group-1/app.py
@@ -1,14 +1,5 @@
+#https://myprojects.up.railway.app/
+
 import streamlit as st
 
-st.title("My First Streamlit App")
-st.write("Hello, CTSS! 👋")
-
-name = st.text_input("What is your name?")
-if name:
-    st.success(f"Welcome, {name}!")
-
-age = st.slider("How old are you?", 10, 20, 15)
-st.write(f"You are {age} years old.")
-
-if st.button("Surprise me"):
-    st.balloons()
+st.write("Hello World I am group 1")

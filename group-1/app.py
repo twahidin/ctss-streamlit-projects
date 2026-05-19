8a7c68b8b9b39c969de30061c4b440d693d094bb
Joe Tay <tay.joe@gmail.com>
2026-05-19 16:31:06 +0800

    group-1: upload from portal at 2026-05-19 08:30 UTC

--- group-1/app.py
+++ group-1/app.py
@@ -1,5 +1,10 @@
 #https://myprojects.up.railway.app/
 
 import streamlit as st
+from database import write_database
 
 st.write("Hello World I am group 1")
+if write_database():
+  st.write("It returns True")
+else:
+  st.write("Nothing is returned")
---
+++ group-1/database.py
@@ -0,0 +1,6 @@
+
+import streamlit as st
+
+def write_database():
+    st.write("I am using this database!")
+    return True

68a02b653ff9598b165bf4bf25561d1d32aaaedc
Joe Tay <tay.joe@gmail.com>
2026-06-27 13:53:45 +0800

    group-8: upload from portal at 2026-06-27 05:53 UTC

--- group-8/app.py
+++ group-8/app.py
@@ -3,8 +3,6 @@
 import json
 from functools import reduce
 import operator
-
-import git
 import os
 
 
@@ -18,6 +16,8 @@ def ReadFromJson(fp, *locations):
       myFile = json.load(f)
   return reduce(operator.getitem, locations, myFile)
 
+
+@st.cache_data
 def WriteToJson(fp:str, value, *locations):
   with open(fp,"r+") as f:
     myFile = json.load(f)
@@ -26,10 +26,33 @@ def WriteToJson(fp:str, value, *locations):
     json.dump(myFile, f, indent = 4)
     f.truncate()
 
-if not os.path.exists("folder"):
-  git.Repo.clone_from("https://github.com/bb-cmyk/osh-kosh.git", "\\folder")
 
-fp = open(os.path.join("folder", "file.json"))
+@st.cache_data
+def writingafileidontwanttobecauseicantputgithublinksintherequirementstxtwhichmakesmefrustratedtoagreatdegree():
+  with open("file.json", "w", encoding="utf-8") as f:
+    json.dump({
+    "11/05/2026": {
+        "1": {
+            "1": {
+                "movie": "Night of the Day of the Dawn of the SOn of the Bride of the Return of the Revenge of the Terror of the Attack of the Evil, Mutant, Alien, Flesh Eating, Hellbound, Zombiefied Living Dead Part 2: In shocking 2D",
+                "seats": {
+                    "A": [0, 0, 0],
+                    "B": [0, 0, 0],
+                    "C": [0, 0, 0]
+                }
+            },
+            "image_link": "https://images.pexels.com/photos/28344947/pexels-photo-28344947.jpeg"
+        }
+    },
+    "ADMINS": {
+        "ADMIN": "1234",
+        "KOSHER": "5678"
+    },
+    "MEMBERS": {
+        "JOHN": {"password": "0000", "tickets": [], "birth date": "01/01/2000", "email": "NaN"},
+        "DOE": {"password": "1111", "tickets": [], "birth date": "02/02/2000", "email": "NaN"}
+    }
+}, f, indent=4)
 #9/5/26
 #Admin Terminal and authentication for backend
 
@@ -52,7 +75,7 @@ if user_name and password and login and uploaded_file:
     #Connect to backend for real authentication
 
 
-    if user_name in ReadFromJson(fp, "ADMINS") and password == ReadFromJson(fp, "ADMINS", user_name): #Testing purposes only
+    if user_name in ReadFromJson("file.json", "ADMINS") and password == ReadFromJson("file.json", "ADMINS", user_name): #Testing purposes only
         st.success('Successfully login!', icon="✅")
     else:
         st.error(f"Unsuccessful login", icon="🚨")
